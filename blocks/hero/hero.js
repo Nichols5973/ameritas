80b42e6c8375cbd9e8fdd15348e430dd5f37f03b
johnnichols <124050236+Nichols5973@users.noreply.github.com>
2024-06-27 15:48:23 -0400

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,10 +1,36 @@
+/**
+ * loads and decorates the hero
+ * @param {Element} block The hero block element
+ */
+
 export default async function decorate(block) {
-  let row = block.firstElementChild;
-  const bg = row.querySelector('picture');
-  block.append(bg);
-  row.remove();
-  const bgP = block.closest('p');
-  if (bgP) bgP.remove();
-  row = block.firstElementChild;
-  row.classList.add('hero-body');
+  const imgBlockChildren = block.children[0];
+  imgBlockChildren.className = 'hero-image-wrapper';
+  [...imgBlockChildren.children].forEach((child) => {
+    const pictureElement = child.querySelectorAll('picture');
+    if (pictureElement.length > 0) {
+      pictureElement.forEach((pic, index) => {
+        if (index === 0) {
+          pic.classList.add('hero-desktop');
+        } else if (index === 1) {
+          pic.classList.add('hero-mobile');
+        }
+        child.className = 'hero-image';
+      });
+    }
+  });
+
+  if (block.children.length > 1) {
+    const textBlockChildren = block.children[1];
+    textBlockChildren.className = 'hero-cta-wrapper';
+    [...textBlockChildren.children].forEach((child) => {
+      child.className = 'hero-textbox';
+    });
+  }
+
+  const imageElements = block.querySelectorAll('img');
+  imageElements.forEach((img) => {
+    img.removeAttribute('loading'); // Lighthouse recommendation: remove lazy-loading
+    img.setAttribute('loading', 'eager');
+  });
 }
